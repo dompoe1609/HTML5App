43e213ceb3ccb5f5ad85193e94d52dc569d88d09
Dominic Pöltl <uekrq@student.kit.edu>
2021-12-05 22:38:24 +0100

    new merge try

--- tetris.js
+++ tetris.js
@@ -252,10 +252,9 @@ updateScore();
 update();
 
 function handleOrientation(event) {
-    updateFieldIfNotNull('Orientation_a', event.alpha);
-    updateFieldIfNotNull('Orientation_b', event.beta);
-    updateFieldIfNotNull('Orientation_g', event.gamma);
-    incrementEventCount();
+    updateFieldIfNotNull(event.alpha);
+    updateFieldIfNotNull(event.beta);
+    updateFieldIfNotNull(event.gamma);
   }
   
   /**
@@ -298,33 +297,39 @@ function handleOrientation(event) {
       }
   }
   
-  function incrementEventCount(){
-    let counterElement = document.getElementById("num-observed-events")
-    let eventCount = parseInt(counterElement.innerHTML)
-    counterElement.innerHTML = eventCount + 1;
-  }
+//   function incrementEventCount(){
+//     let counterElement = document.getElementById("num-observed-events")
+//     let eventCount = parseInt(counterElement.innerHTML)
+//     counterElement.innerHTML = eventCount + 1;
+//   }
   
-  function updateFieldIfNotNull(fieldName, value, precision=10){
-    if (value != null)
-      document.getElementById(fieldName).innerHTML = value.toFixed(precision);
-  }
+  function updateFieldIfNotNull(value, precision=10){
+    if (value != null) {
+        if (value.toFixed(precision) >= 210.0){
+            playerMove(1);
+        } else if (value.toFixed(precision) <= 160) {
+            playerMove(-1);
+        }
+    }
+
+  };
   
-  function handleMotion(event) {
-    updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
-    updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
-    updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
+//   function handleMotion(event) {
+//     updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
+//     updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
+//     updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
   
-    updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
-    updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
-    updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
+//     updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
+//     updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
+//     updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
   
-    updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
+//     updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
   
-    updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
-    updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
-    updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
-    incrementEventCount();
-  }
+//     updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
+//     updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
+//     updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
+//     incrementEventCount();
+//   }
   
   function firstClick() {
       requestDeviceMotion(function(err) {
